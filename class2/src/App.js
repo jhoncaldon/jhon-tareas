e6c438804a6ef98f88841d7e035cc9e64abf8130
jhon caldon <brandiny92@gmail.com>
2024-07-31 12:12:31 -0500

    desarrollo de la tarea 2

---
+++ class2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PruebaComponet from './componet/pruebacomponet';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+     </h1>
+     <PruebaComponet/>
+    </div>
+  );
+}
+
+export default App;
---
+++ class2/src/componet/pruebacomponet.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+function App() {
+  return (
+    <div className='fondocolorido'>
+
+      <h1>Bryan Tracy</h1>
+      <img src="https://pbs.twimg.com/media/C8f8CQgWAAE_kxt?format=jpg&name=medium" alt= "tracy" 
+      style={{width: '500px', height: 'auto'}}/>
+      <p>
+      Brian Tracy es un orador motivacional, escritor de ventas y desarrollo personal y empresario en el rubro. Ha escrito más de setenta libros que se han traducido a más de una docena de idiomas​ Sus libros más populares son ¡Gana lo que realmente vales!, ¡Tráguese ese sapo! y La psicología del logro
+      </p>
+    </div>
+  );
+}
+
+export default App;
